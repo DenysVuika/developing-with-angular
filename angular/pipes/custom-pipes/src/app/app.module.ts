422b1e1c69dcd62257d4f076479342aac63d4e7a
Denys Vuika <denis.vuyka@gmail.com>
2017-05-29 20:19:30 +0100

    fileSize pipe

--- angular/pipes/custom-pipes/src/app/app.module.ts
+++ angular/pipes/custom-pipes/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FileSizePipe } from './pipes/file-size.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileSizePipe
   ],
   imports: [
     BrowserModule,
