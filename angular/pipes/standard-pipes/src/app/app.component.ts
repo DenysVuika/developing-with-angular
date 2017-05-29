13505a43aeffb37a6a302f4efed6e80526a9cecf
Denys Vuika <denis.vuyka@gmail.com>
2017-05-29 16:09:40 +0100

    async pipe examples

--- angular/pipes/standard-pipes/src/app/app.component.ts
+++ angular/pipes/standard-pipes/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Standard pipes';
   currentDate: number = Date.now();
   birthday = new Date(1879, 3, 14);
@@ -18,6 +19,7 @@ export class AppComponent {
   };
   numberCollection = [0, 1, 2, 3, 4, 5, 6];
 
+  // I18nSelect
   titleMode = 'primary';
   titleValues = {
     primary: 'This is a primary title',
@@ -25,6 +27,7 @@ export class AppComponent {
     other: 'This is an alternative title'
   };
 
+  // I18nSelect
   currentLanguage = 'en-GB';
   buttonLabels = {
     'en-US': 'Pick color',
@@ -33,6 +36,7 @@ export class AppComponent {
     'other': 'Pick colour'
   };
 
+  // I18nPluralPipe
   posts = [
     {
       content: 'Post 1 content',
@@ -52,4 +56,31 @@ export class AppComponent {
     '=1': 'There is one comment for this post.',
     'other': 'There are # comments for this post.'
   };
+
+  // Async
+  comments: Observable<string[]>;
+
+  checkComments() {
+    this.comments = new Observable(observer => {
+      observer.next([
+        'Comment 1',
+        'Comment 2',
+        'Comment 3'
+      ]);
+    }).delay(1000);
+  }
+
+  resetComments() {
+    this.comments = null;
+  }
+
+  // Async (date/time)
+  currentTime: Observable<Date>;
+
+  ngOnInit() {
+    this.currentTime = new Observable<Date>(observer => {
+      setInterval(_ => observer.next(new Date()), 1000);
+    });
+  }
+
 }
