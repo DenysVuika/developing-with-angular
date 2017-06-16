83cd7613ab993f8ee03a5e9820ff48c7548b26e4
Denys Vuika <denis.vuyka@gmail.com>
2017-06-16 22:17:15 +0100

    footer component

--- angular/components/basic-components/src/app/app.module.ts
+++ angular/components/basic-components/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
