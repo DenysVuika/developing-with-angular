823f9b5f9f87b93de756f8e474ea081de0b5d4d2
Denys Vuika <denis.vuyka@gmail.com>
2017-06-21 17:57:31 +0100

    update header component

--- angular/components/basic-components/src/app/components/header.component.ts
+++ angular/components/basic-components/src/app/components/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: [
+    './header.component.css'
+  ]
 })
 export class HeaderComponent {
-
   title: string = 'Header';
-
 }
