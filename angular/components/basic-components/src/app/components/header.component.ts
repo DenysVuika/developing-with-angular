f9a92e79e177a6b21252442adbf1c4c63b6cb96e
Denys Vuika <denis.vuyka@gmail.com>
2017-06-22 19:22:05 +0100

    header title binding

--- angular/components/basic-components/src/app/components/header.component.ts
+++ angular/components/basic-components/src/app/components/header.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: [
-    './header.component.css'
-  ]
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  @Input()
   title: string = 'Header';
 }
