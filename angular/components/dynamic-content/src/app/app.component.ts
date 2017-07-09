35208d654c3a3e1648abf80085c3f6759e3de9f7
Denys Vuika <denis.vuyka@gmail.com>
2017-07-09 12:13:22 +0100

    R03. Advanced Angular: Dynamic Content (#16)
    * 'dynamic content' project
    
    * templated list example
    
    * dynamic content example
    
    * runtime content example

---
+++ angular/components/dynamic-content/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  listItems: Array<{title: string}> = [
+    { title: 'item 1' },
+    { title: 'item 2' },
+    { title: 'item 3' }
+  ]
+
+  context: { text: string } = {
+      text: 'test'
+  }
+}
---
+++ angular/components/dynamic-content/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TemplatedListComponent } from './templated-list/templated-list.component';
+import {
+  DynamicContentComponent,
+  DynamicSample1Component,
+  DynamicSample2Component,
+  UnknownDynamicComponent
+} from './dynamic-content/dynamic-content.component';
+import { RuntimeContentComponent } from './runtime-content/runtime-content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplatedListComponent,
+    DynamicContentComponent,
+    DynamicSample1Component,
+    DynamicSample2Component,
+    UnknownDynamicComponent,
+    RuntimeContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  entryComponents: [
+    DynamicSample1Component,
+    DynamicSample2Component,
+    UnknownDynamicComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
