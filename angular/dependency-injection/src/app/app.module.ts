324f16865df3148780e368502dc54ebb57e96290
Denys Vuika <denis.vuyka@gmail.com>
2017-08-05 13:17:00 +0100

    simple components

--- angular/dependency-injection/src/app/app.module.ts
+++ angular/dependency-injection/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Component2Component
   ],
   imports: [
     BrowserModule
