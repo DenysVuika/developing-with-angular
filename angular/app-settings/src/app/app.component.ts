eea562ef4f8881da19910e7a545762f5803f2a57
Denys Vuika <denis.vuyka@gmail.com>
2017-09-16 18:39:53 +0100

    app config service example

--- angular/app-settings/src/app/app.component.ts
+++ angular/app-settings/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppConfigService } from './app-config.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(appConfig: AppConfigService) {
+    this.title = appConfig.data.title;
+  }
 }
--- angular/app-settings/src/app/app.module.ts
+++ angular/app-settings/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppConfigService } from './app-config.service';
+
+export function setupAppConfigServiceFactory(service: AppConfigService): Function {
+  return () => service.load();
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    AppConfigService,
+    {
+        provide: APP_INITIALIZER,
+        useFactory: setupAppConfigServiceFactory,
+        deps: [
+            AppConfigService
+        ],
+        multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
