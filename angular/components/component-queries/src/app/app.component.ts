551191efe079f816afa186426c14558ee4f6d120
Denys Vuika <denis.vuyka@gmail.com>
2017-10-15 13:57:49 +0100

    component queries examples

---
+++ angular/components/component-queries/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild(ListComponent)
+  list: ListComponent;
+
+  @ViewChildren(ListComponent)
+  lists: QueryList<ListComponent>;
+
+  ngAfterViewInit() {
+    let i = 0;
+    this.lists.forEach(l => {
+      l.title = 'Custom title #' + (i++);
+    });
+  }
+}
---
+++ angular/components/component-queries/src/app/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, ContentChild, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+import { ListItemComponent } from '../list-item/list-item.component';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements AfterContentInit {
+
+  @Input()
+  title = 'List Title';
+
+  @ContentChild(ListItemComponent)
+  firstListItem: ListItemComponent;
+
+  @ContentChildren(ListItemComponent)
+  listItems: QueryList<ListItemComponent>;
+
+  ngAfterContentInit() {
+    this.listItems.forEach(item => {
+      item.title = item.title + ' (' + new Date().toLocaleDateString() + ')';
+    });
+  }
+}
