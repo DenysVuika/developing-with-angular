52a4e59e3dc634d66d439998a79715f9eb97e42d
Denys Vuika <denis.vuyka@gmail.com>
2017-11-04 17:02:14 +0000

    ngOnChanges example

--- angular/components/lifecycle/src/app/app.module.ts
+++ angular/components/lifecycle/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { OnChangesDemoComponent } from './on-changes-demo/on-changes-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnChangesDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
