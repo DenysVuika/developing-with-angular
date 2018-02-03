25112021c49df1cb34fae260d0331a8bf0beeb20
Denys Vuika <denis.vuyka@gmail.com>
2018-02-03 12:07:34 +0000

    sample page components

--- angular/plugins/src/app/app.module.ts
+++ angular/plugins/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
