cc80d18817fd062d72522a2ceb44d83ad3e40c49
Denys Vuika <denys.vuika@gmail.com>
2018-10-23 14:59:22 +0100

    upgrade: lifecycle

--- angular/components/lifecycle/src/app/after-view-init-demo/after-view-init-demo.component.spec.ts
+++ angular/components/lifecycle/src/app/after-view-init-demo/after-view-init-demo.component.spec.ts
@@ -8,15 +8,13 @@ describe('AfterViewInitDemoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AfterViewInitDemoComponent ]
-    })
-    .compileComponents();
+      declarations: [AfterViewInitDemoComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AfterViewInitDemoComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
--- angular/components/lifecycle/src/app/app.component.spec.ts
+++ angular/components/lifecycle/src/app/app.component.spec.ts
@@ -1,27 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OnChangesDemoComponent } from './on-changes-demo/on-changes-demo.component';
+import { DoCheckDemoComponent } from './do-check-demo/do-check-demo.component';
+import { OnInitDemoComponent } from './on-init-demo/on-init-demo.component';
+import { AfterContentInitChildComponent } from './after-content-init-child/after-content-init-child.component';
+import { AfterContentInitDemoComponent } from './after-content-init-demo/after-content-init-demo.component';
+import { OnDestroyDemoComponent } from './on-destroy-demo/on-destroy-demo.component';
+import { AfterViewInitDemoComponent } from './after-view-init-demo/after-view-init-demo.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [CommonModule, FormsModule],
       declarations: [
-        AppComponent
-      ],
+        AppComponent,
+        OnInitDemoComponent,
+        OnChangesDemoComponent,
+        DoCheckDemoComponent,
+        OnInitDemoComponent,
+        AfterContentInitChildComponent,
+        AfterContentInitDemoComponent,
+        OnDestroyDemoComponent,
+        AfterViewInitDemoComponent
+      ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
