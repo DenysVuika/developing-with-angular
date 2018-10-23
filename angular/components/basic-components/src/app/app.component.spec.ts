512b7e536b9faa7b40670911ac45377181d3075c
Denys Vuika <denys.vuika@gmail.com>
2018-10-23 12:29:38 +0100

    upgrade: basic-components

--- angular/components/basic-components/src/app/app.component.spec.ts
+++ angular/components/basic-components/src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, HeaderComponent, FooterComponent]
     }).compileComponents();
   }));
 
@@ -21,12 +21,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
   }));
 });
