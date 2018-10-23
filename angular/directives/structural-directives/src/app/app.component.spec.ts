988e6bd3f2e984e921864b13feeb11f7c2c837a3
Denys Vuika <denys.vuika@gmail.com>
2018-10-23 17:31:54 +0100

    upgrade: structural-directives

--- angular/directives/structural-directives/src/app/app.component.spec.ts
+++ angular/directives/structural-directives/src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RepeatDirective } from './repeat.directive';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [CommonModule, FormsModule],
+      declarations: [AppComponent, RepeatDirective]
     }).compileComponents();
   }));
 
@@ -15,18 +17,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
 });
