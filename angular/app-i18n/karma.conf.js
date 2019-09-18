e6c653a465eca79c2005214d3dec07bee15413f0
Denys Vuika <denys.vuika@gmail.com>
2019-09-18 09:33:32 +0100

    update configuration

--- angular/app-i18n/karma.conf.js
+++ angular/app-i18n/karma.conf.js
@@ -1,5 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function(config) {
   config.set({
