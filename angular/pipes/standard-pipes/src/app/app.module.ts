4a625fcabeac0cac6f0d261d107f4ddb04d1e495
Denys Vuika <denys.vuika@gmail.com>
2020-04-16 18:31:23 +0100

    upgrade standard-pipes

--- angular/pipes/standard-pipes/src/app/app.module.ts
+++ angular/pipes/standard-pipes/src/app/app.module.ts
@@ -1,20 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
