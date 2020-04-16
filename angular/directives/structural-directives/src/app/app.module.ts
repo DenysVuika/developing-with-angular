a8ce5296cb11453347034eb2b97f70d5f5712e65
Denys Vuika <denys.vuika@gmail.com>
2020-04-16 17:59:27 +0100

    upgrade structural-directives

--- angular/directives/structural-directives/src/app/app.module.ts
+++ angular/directives/structural-directives/src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { RepeatDirective } from './repeat.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RepeatDirective
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, RepeatDirective],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
