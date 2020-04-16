afe39e82cc7314f1c2fa335c529736615b1cf3ce
Denys Vuika <denys.vuika@gmail.com>
2020-04-16 18:22:29 +0100

    upgrade pure-impure

--- angular/pipes/pure-impure/src/app/app.module.ts
+++ angular/pipes/pure-impure/src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PublicPostsPipe } from './pipes/public-posts.pipe';
 import { PublicPostsImpurePipe } from './pipes/public-posts-impure.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PublicPostsPipe,
-    PublicPostsImpurePipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PublicPostsPipe, PublicPostsImpurePipe],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
