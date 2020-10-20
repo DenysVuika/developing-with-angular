463103338eccd805831ba282ca1818a2cdae7dc5
Denys Vuika <denys.vuika@gmail.com>
2020-10-20 18:41:35 +0100

    remove unused libs and settings

--- angular/app-i18n/karma.conf.js
+++ angular/app-i18n/karma.conf.js
@@ -1,9 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
-
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -12,15 +9,15 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
     },
 
     reporters: ['progress', 'kjhtml'],
@@ -36,10 +33,10 @@ module.exports = function(config) {
           '--no-sandbox',
           '--headless',
           '--disable-gpu',
-          '--remote-debugging-port=9222'
-        ]
-      }
+          '--remote-debugging-port=9222',
+        ],
+      },
     },
-    singleRun: false
+    singleRun: false,
   });
 };
