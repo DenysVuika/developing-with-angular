e2ee774af3f0ad476dccaa19ef0d9abf3a3a600e
Denys Vuika <denys.vuika@gmail.com>
2023-03-05 21:52:50 +0000

    code fixes

--- if-unchanged.js
+++ if-unchanged.js
@@ -10,7 +10,7 @@ const currentPath = path.join(
 console.log('path: ', currentPath);
 
 const result = child_process
-  .execSync('git diff --name-only origin/master... | cat')
+  .execSync('git diff --name-only origin/master... -- | cat')
   .toString();
 
 if (result) {
@@ -18,8 +18,8 @@ if (result) {
 
   const dirs = result
     .split('\n')
-    .filter(file => file)
-    .map(file => path.join(__dirname, file));
+    .filter((file) => file)
+    .map((file) => path.join(__dirname, file));
 
   for (const dir of dirs) {
     if (dir.startsWith(currentPath)) {
