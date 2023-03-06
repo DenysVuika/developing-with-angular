292c15b0268b12d5e5649bd821a534cc649bbaed
Denys Vuika <denys.vuika@gmail.com>
2023-03-06 22:39:33 +0000

    ng15: app-attach-directive

--- angular/directives/app-attach-directive/src/app/app.component.spec.ts
+++ angular/directives/app-attach-directive/src/app/app.component.spec.ts
@@ -1,18 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { AttachDirective } from './attach.directive';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [CommonModule],
       declarations: [AppComponent, AttachDirective]
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
