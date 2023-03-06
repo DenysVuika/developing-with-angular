0d632513798b1bb91c36a8bd7f2daf0ab77bc4dc
Denys Vuika <denys.vuika@gmail.com>
2023-03-06 12:16:47 +0000

    ng15

--- angular/app-settings/src/app/app.component.spec.ts
+++ angular/app-settings/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppConfigService } from './app-config.service';
@@ -6,7 +6,7 @@ import { AppConfigService } from './app-config.service';
 describe('AppComponent', () => {
   let service: AppConfigService;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
       declarations: [AppComponent]
@@ -16,13 +16,13 @@ describe('AppComponent', () => {
     service.data.title = 'Test App';
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h1 tag', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
