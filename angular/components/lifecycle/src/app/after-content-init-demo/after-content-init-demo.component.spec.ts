337c99fd5f1e656e958667110dba3907f2cfe2a5
Denys Vuika <denys.vuika@gmail.com>
2023-03-06 20:42:16 +0000

    migrate lifecycle to ng15

--- angular/components/lifecycle/src/app/after-content-init-demo/after-content-init-demo.component.spec.ts
+++ angular/components/lifecycle/src/app/after-content-init-demo/after-content-init-demo.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AfterContentInitDemoComponent } from './after-content-init-demo.component';
 
@@ -6,7 +6,7 @@ describe('AfterContentInitDemoComponent', () => {
   let component: AfterContentInitDemoComponent;
   let fixture: ComponentFixture<AfterContentInitDemoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AfterContentInitDemoComponent ]
     })
--- angular/components/lifecycle/src/app/on-destroy-demo/on-destroy-demo.component.spec.ts
+++ angular/components/lifecycle/src/app/on-destroy-demo/on-destroy-demo.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { OnDestroyDemoComponent } from './on-destroy-demo.component';
 
@@ -6,7 +6,7 @@ describe('OnDestroyDemoComponent', () => {
   let component: OnDestroyDemoComponent;
   let fixture: ComponentFixture<OnDestroyDemoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ OnDestroyDemoComponent ]
     })
