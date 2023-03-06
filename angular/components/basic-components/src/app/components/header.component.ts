e94b3d7fd022d4e3fc4a77a9f0df88194da92cac
Denys Vuika <denys.vuika@gmail.com>
2023-03-06 11:03:02 -0500

    Ng15 basic components (#17911)
    * ng15
    
    * upgrade configs
    
    * upgrade libs
    
    * audit fix
    
    * upgrade to angular 15
    
    * fix package script

--- angular/components/basic-components/src/app/components/header.component.ts
+++ angular/components/basic-components/src/app/components/header.component.ts
@@ -3,15 +3,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
   @Input()
   title = 'Header';
 
   @Output()
-  titleClick = new EventEmitter();
+  titleClick = new EventEmitter<void>();
 
   handleTitleClick() {
     this.titleClick.next();
