cba1aaa5b9f91beef7fc3b48ace52d1f6096ed7d
Denys Vuika <denys.vuika@gmail.com>
2023-03-07 15:11:04 -0500

    ng15: dependency injection (#17918)

--- angular/dependency-injection/src/app/app.component.spec.ts
+++ angular/dependency-injection/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { Component1Component } from './component1/component1.component';
@@ -9,14 +9,14 @@ export function dateFactory() {
 }
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent, Component1Component, Component2Component],
       providers: [{ provide: 'DATE_NOW', useFactory: dateFactory }]
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
--- angular/dependency-injection/src/app/component2/component2.component.spec.ts
+++ angular/dependency-injection/src/app/component2/component2.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { Component2Component } from './component2.component';
 
@@ -6,7 +6,7 @@ describe('Component2Component', () => {
   let component: Component2Component;
   let fixture: ComponentFixture<Component2Component>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ Component2Component ]
     })
