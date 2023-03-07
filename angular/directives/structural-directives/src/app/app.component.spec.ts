f337738997d6edc5d587ba0a2a2a891410b5b154
Denys Vuika <denys.vuika@gmail.com>
2023-03-07 00:20:34 +0000

    ng15: structural-directives

--- angular/directives/structural-directives/src/app/app.component.spec.ts
+++ angular/directives/structural-directives/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
@@ -6,14 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { RepeatDirective } from './repeat.directive';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [CommonModule, FormsModule],
       declarations: [AppComponent, RepeatDirective]
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
