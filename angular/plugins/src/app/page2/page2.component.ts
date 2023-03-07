3fb9665df68f7305b3dba49c663b69a40797c97f
Denys Vuika <denys.vuika@gmail.com>
2023-03-07 22:06:54 +0000

    migrate to eslint

--- angular/plugins/src/app/page2/page2.component.ts
+++ angular/plugins/src/app/page2/page2.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
-  styleUrls: ['./page2.component.css']
+  styleUrls: ['./page2.component.css'],
 })
-export class Page2Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class Page2Component {}
